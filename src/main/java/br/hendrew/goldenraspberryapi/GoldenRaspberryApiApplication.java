5a6f08870e4e5f75cf115ebdef426b086fbdb532
HendrewMartins <46753193+HendrewMartins@users.noreply.github.com>
2025-08-16 11:43:59 -0300

    Initial

---
+++ src/main/java/br/hendrew/goldenraspberryapi/GoldenRaspberryApiApplication.java
@@ -0,0 +1,13 @@
+package br.hendrew.goldenraspberryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenRaspberryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/hendrew/goldenraspberryapi/GoldenRaspberryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.hendrew.goldenraspberryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
